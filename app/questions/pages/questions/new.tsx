66eef3f6e9b0e2468b89ed42fb2f5a53c186bfbf
kaznak <kaznak.at.work@gmail.com>
2021-02-14 16:44:21 +0900

    setup basic listing and mutation

--- app/questions/pages/questions/new.tsx
+++ app/questions/pages/questions/new.tsx
@@ -13,9 +13,20 @@ const NewQuestionPage: BlitzPage = () => {
 
       <QuestionForm
         initialValues={{}}
-        onSubmit={async () => {
+        onSubmit={async (event) => {
           try {
-            const question = await createQuestionMutation({ data: { name: "MyName" } })
+            const question = await createQuestionMutation({
+              data: {
+                text: event.target[0].value,
+                choices: {
+                  create: [
+                    { text: event.target[1].value },
+                    { text: event.target[2].value },
+                    { text: event.target[3].value },
+                  ],
+                },
+              },
+            })
             alert("Success!" + JSON.stringify(question))
             router.push(`/questions/${question.id}`)
           } catch (error) {
